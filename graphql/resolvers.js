284f0e729c627e606908041ff684e84edaa727b1
Guillem Cordoba <guillem.cordoba@gmail.com>
2020-10-07 17:09:32 +0200

    Updates

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,49 +1,47 @@
 function hashToString(hash) {
     return hash.hash_type.toString('hex') + hash.hash.toString('hex');
 }
-export const profilesUsernameResolvers = (appWebsocket, cellId, zomeName = 'profiles') => ({
-    Query: {
-        async allAgents(_, __) {
-            const allAgents = await appWebsocket.callZome({
-                cap: null,
-                cell_id: cellId,
-                zome_name: zomeName,
-                fn_name: 'get_all_profiles',
-                payload: null,
-                provenance: cellId[1],
-            });
-            return allAgents.map((agent) => ({
-                id: hashToString(agent.agent_id),
-                username: agent.username,
-            }));
+export function profilesUsernameResolvers(appWebsocket, cellId, zomeName = 'profiles') {
+    function callZome(fn_name, payload) {
+        return appWebsocket.callZome({
+            cap: null,
+            cell_id: cellId,
+            zome_name: zomeName,
+            fn_name: fn_name,
+            payload: payload,
+            provenance: cellId[1],
+        });
+    }
+    return {
+        Agent: {
+            async username(parent) {
+                if (parent.username)
+                    return parent.username;
+                return callZome('get_agent_username', { agent_address: parent.id });
+            },
         },
-        async me(_, __) {
-            const profile = await appWebsocket.callZome({
-                cap: null,
-                cell_id: cellId,
-                zome_name: zomeName,
-                fn_name: 'get_my_profile',
-                payload: null,
-                provenance: cellId[1],
-            });
-            return profile;
+        Query: {
+            async allAgents(_, __) {
+                const allAgents = await callZome('get_all_profiles', null);
+                return allAgents.map((agent) => ({
+                    id: hashToString(agent.agent_id),
+                    username: agent.username,
+                }));
+            },
+            async me(_, __) {
+                const profile = await callZome('get_my_profile', null);
+                return profile;
+            },
         },
-    },
-    Mutation: {
-        async setUsername(_, { username }) {
-            const agent = await appWebsocket.callZome({
-                cap: null,
-                cell_id: cellId,
-                zome_name: zomeName,
-                fn_name: 'create_profile',
-                payload: { username },
-                provenance: cellId[1],
-            });
-            return {
-                id: hashToString(cellId[1]),
-                username,
-            };
+        Mutation: {
+            async setUsername(_, { username }) {
+                const agent = await callZome('create_profile', { username });
+                return {
+                    id: hashToString(cellId[1]),
+                    username,
+                };
+            },
         },
-    },
-});
+    };
+}
 //# sourceMappingURL=resolvers.js.map
